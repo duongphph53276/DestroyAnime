dd23eaec5c8635c34ac0ee0bd7fa4f597780f835
duongphph53276 <duongphph53276@gmail.com>
2025-03-19 05:37:42 +0700

    more helmet

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 const Welcome = () => {
   return (
+    <>
+    <Helmet>
+        Destroy Anime
+    </Helmet>
     <div>
       Dự án webgame-base : Destroy Anime
     </div>
+    </>
   )
 }
 
--- src/components/admin/Dashboard.tsx
+++ src/components/admin/Dashboard.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 const Dashboard = () => {
   return (
+    <>
+    <Helmet>
+        Admin
+    </Helmet>
     <div>
       Dashboard home
     </div>
+    </>
   )
 }
 
